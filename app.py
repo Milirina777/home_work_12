c7ba84bc53d0d980edb03637ac505c3d7caa4239
Milirina777 <116578855+Milirina777@users.noreply.github.com>
2022-11-13 05:04:07 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,21 @@
-from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+import logging
 
-POST_PATH = "posts.json"
-UPLOAD_FOLDER = "uploads/images"
+from flask import Flask, send_from_directory
+
+from loader.views import loader_blueprint
+from main.views import main_blueprint
+
 
 app = Flask(__name__)
 
+app.register_blueprint(main_blueprint)
+app.register_blueprint(loader_blueprint)
+logging.basicConfig(filename="file_of_logs.log", level=logging.INFO)
 
-@app.route("/")
-def page_index():
-    pass
-
-
-@app.route("/list")
-def page_tag():
-    pass
-
-
-@app.route("/post", methods=["GET", "POST"])
-def page_post_form():
-    pass
-
-
-@app.route("/post", methods=["POST"])
-def page_post_upload():
-    pass
-
-
+# запускает работу основных файлов
 @app.route("/uploads/<path:path>")
 def static_dir(path):
     return send_from_directory("uploads", path)
 
 
-app.run()
-
+app.run(host="127.0.0.1", port=8080, debug=True)
--- functions.py
+++ functions.py
@@ -1,1 +1,43 @@
 import json
+import logging
+
+
+def load_posts():
+    """Загружает список из файла"""
+
+    with open("posts.json", 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
+
+
+def get_posts_by_combination_simbols(combination_simbols):
+    """Возвращает пост по введенной комбинации символов"""
+    data = load_posts()
+    result = []
+
+    for post in data:
+        if combination_simbols.lower() in post["content"].lower():
+            result.append(post)
+
+    return result
+
+
+def save_picture(picture):
+    """Сохраняет изображение"""
+    filename = picture.filename
+
+# путь по которому можно работать с картинкой
+    picture.save(f'./uploads/{filename}')
+    return f'uploads/{filename}'
+
+
+def add_post(post):
+    """Возвращает добавленный пост, предварительно добавив его в файл json"""
+    data_posts = load_posts()
+    data_posts.append(post)
+
+    with open("posts.json", 'w', encoding='utf-8') as file:
+        json.dump(data_posts, file)
+    return post
+
+
